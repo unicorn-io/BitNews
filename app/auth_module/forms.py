12acf46fd18292f249d5929a03a57ce40b480a9b
shanuma1 <shanuma1@binghamton.edu>
2020-11-03 12:38:33 +0530

    Added Recapthca verification on login and register forms

--- app/auth_module/forms.py
+++ app/auth_module/forms.py
@@ -1,10 +1,12 @@
 # Import Form and RecaptchaField
-from flask_wtf import Form, RecaptchaField
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import TextField, PasswordField, BooleanField
 from wtforms.validators import Required, Email, EqualTo
 
 # define the login form (WTFforms)
 
-class LoginForm(Form):
-    email = TextField('Email Address', [Email(), Required(message='Forgot your email address?')])
+class LoginForm(FlaskForm):
+    username = TextField('Username', [Email(), Required(message='Forgot your username?')])
     password = PasswordField("Password", [Required(message='Must provide a password. ;-)')])
+    
+

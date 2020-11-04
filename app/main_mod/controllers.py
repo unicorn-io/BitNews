a93e9d5014f5c162be9690b9e8c3ce92a4d7d31e
DhruvRaipure <dhruvraipure@gmail.com>
2020-11-05 01:57:02 +0530

    blog added

--- app/main_mod/controllers.py
+++ app/main_mod/controllers.py
@@ -6,3 +6,12 @@ main_mod = Blueprint('main', __name__, url_prefix="/")
 @main_mod.route('/')
 def index():
     return render_template('index.html')
+@main_mod.route('/contact')
+def contact():
+    return render_template('contact.html')
+@main_mod.route('/post')
+def post():
+    return render_template('regular.html')
+@main_mod.route('/blog')
+def blog():
+    return render_template('blog.html')
